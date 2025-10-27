08eb07555feffd75b42b587b507dc5e68df20118
KAPILKASHYAP-99 <kapilkashyap.inwork@gmail.com>
2025-10-27 13:28:58 +0530

    machine-coding round

--- Throttling/index.js
+++ Throttling/index.js
@@ -19,3 +19,13 @@ const handleScroll = () => {
 const throttledScroll = throttle(handleScroll, 500);
 
 container.addEventListener("scroll", throttledScroll);
+
+/**
+ * throttling is a techinque used to limit how ofter a function runs over time --
+ * even it's being called repeatedly
+ * why throttling used==
+ * 1) performace optimization - prevent slowdown
+ * 2) make animation or scrolling smoother
+ * 3) useful in api to prevent spamming or rate limit request
+ * 4) 
+ */
